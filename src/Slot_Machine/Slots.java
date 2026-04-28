092c5921fb811ee00c6f573431bf54c5223c5957
biggs2024 <ksandlin2021@gmail.com>
2026-04-28 01:42:57 -0400

    Added animation affects to images

--- src/Slot_Machine/Slots.java
+++ src/Slot_Machine/Slots.java
@@ -1,6 +1,7 @@
 package Slot_Machine;
 
 import javafx.animation.KeyFrame;
+import javafx.animation.PauseTransition;
 import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
@@ -13,18 +14,31 @@ import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.net.URL;
+
 public class Slots extends Application{
-	// String array to house absolute file paths of images, will need to update eventually
-	String[] images = { "file:src/Slot_Machine/Dollar.JPG", "file:src/Slot_Machine/Strawberry.JPG", "file:src/Slot_Machine/Watermelon.JPG" };
+	// Resource names bundled in the application package
+	String[] imageNames = {
+			"Dollar.JPG",
+			"Strawberry.JPG",
+			"Watermelon.JPG"
+	};
+
+	// Preload images so each spin frame does not re-read files from disk
+	Image[] images = {
+			requireImage(imageNames[0]),
+			requireImage(imageNames[1]),
+			requireImage(imageNames[2])
+	};
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// Creation of javafx components
 		HBox display = new HBox();
 		HBox slotWindow = new HBox();
 		Button spin = new Button("SPIN");
-		ImageView slotOneView = new ImageView(new Image(images[0]));
-		ImageView slotTwoView = new ImageView(new Image(images[0]));
-		ImageView slotThreeView = new ImageView(new Image(images[0]));
+		ImageView slotOneView = new ImageView(images[0]);
+		ImageView slotTwoView = new ImageView(images[0]);
+		ImageView slotThreeView = new ImageView(images[0]);
 		
 		// Adding nodes to panes
 		slotWindow.getChildren().addAll(slotOneView, slotTwoView, slotThreeView);
@@ -34,9 +48,7 @@ public class Slots extends Application{
 		Scene scene = new Scene(display);
 		
 		// Action event for when button is clicked
-		spin.setOnAction( e -> {
-			
-		});
+		spin.setOnAction(e -> animationPlayer(slotOneView, slotTwoView, slotThreeView, spin, 2.0));
 		
 		// Display scene
 		primaryStage.setTitle("Slot Machine");
@@ -48,16 +60,38 @@ public class Slots extends Application{
 		launch(args);
 	}
 	
-	// Animation to cycle through images for 2 seconds WIP
-	public void animationPlayer(ImageView v1, ImageView v2, ImageView v3) {
-		EventHandler<ActionEvent> eventHandler = e -> {
-			v1.setImage(new Image(images[(int)(Math.random() * 3)]));
-			v2.setImage(new Image(images[(int)(Math.random() * 3)]));
-			v3.setImage(new Image(images[(int)(Math.random() * 3)]));
+	// Cycle images for spinSeconds, then stop on random final symbols
+	public void animationPlayer(ImageView v1, ImageView v2, ImageView v3, Button spinButton, double spinSeconds) {
+		spinButton.setDisable(true);
+
+		EventHandler<ActionEvent> cycleFrame = e -> {
+			v1.setImage(images[(int) (Math.random() * images.length)]);
+			v2.setImage(images[(int) (Math.random() * images.length)]);
+			v3.setImage(images[(int) (Math.random() * images.length)]);
 		};
-		
-		Timeline animation = new Timeline(new KeyFrame(
-				Duration.seconds(3), eventHandler));
+
+		// Update every 100ms for a slot-machine look
+		Timeline spinTimeline = new Timeline(new KeyFrame(Duration.millis(100), cycleFrame));
+		spinTimeline.setCycleCount(Timeline.INDEFINITE);
+		spinTimeline.play();
+
+		PauseTransition stopAfter = new PauseTransition(Duration.seconds(spinSeconds));
+		stopAfter.setOnFinished(e -> {
+			spinTimeline.stop();
+			v1.setImage(images[(int) (Math.random() * images.length)]);
+			v2.setImage(images[(int) (Math.random() * images.length)]);
+			v3.setImage(images[(int) (Math.random() * images.length)]);
+			spinButton.setDisable(false);
+		});
+		stopAfter.play();
+	}
+
+	private Image requireImage(String imageName) {
+		URL resource = getClass().getResource(imageName);
+		if (resource == null) {
+			throw new IllegalArgumentException("Missing bundled image in Slot_Machine package: " + imageName);
+		}
+		return new Image(resource.toExternalForm());
 	}
 
 }
