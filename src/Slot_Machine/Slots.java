6d00680a1f016d37e0425379ce0f541a0c3b5080
biggs2024 <ksandlin2021@gmail.com>
2026-05-05 09:28:09 -0400

    Put on Back burner for now, just redesigned layout

--- src/Slot_Machine/Slots.java
+++ src/Slot_Machine/Slots.java
@@ -6,10 +6,13 @@ import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -19,33 +22,41 @@ import java.net.URL;
 public class Slots extends Application{
 	// Resource names bundled in the application package
 	String[] imageNames = {
-			"Dollar.JPG",
-			"Strawberry.JPG",
-			"Watermelon.JPG"
+			"cherry.jpg",
+			"gem.jpg",
+			"orange.jpg",
+			"seven.jpg"
 	};
 
 	// Preload images so each spin frame does not re-read files from disk
 	Image[] images = {
 			requireImage(imageNames[0]),
 			requireImage(imageNames[1]),
-			requireImage(imageNames[2])
+			requireImage(imageNames[2]),
+			requireImage(imageNames[3])
 	};
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// Creation of javafx components
-		HBox display = new HBox();
-		HBox slotWindow = new HBox();
+		BorderPane root = new BorderPane();
+		GridPane viewWindow = new GridPane();
 		Button spin = new Button("SPIN");
-		ImageView slotOneView = new ImageView(images[0]);
-		ImageView slotTwoView = new ImageView(images[0]);
-		ImageView slotThreeView = new ImageView(images[0]);
+		ImageView slotOneView = imageResize(new ImageView(images[0]));
+		ImageView slotTwoView = imageResize(new ImageView(images[0]));
+		ImageView slotThreeView = imageResize(new ImageView(images[0]));
 		
-		// Adding nodes to panes
-		slotWindow.getChildren().addAll(slotOneView, slotTwoView, slotThreeView);
-		display.getChildren().addAll(spin, slotWindow);
+		viewWindow.add(slotOneView, 0, 0);
+		viewWindow.add(slotTwoView, 1, 0);
+		viewWindow.add(slotThreeView, 2, 0);
+		
+		root.setCenter(viewWindow);
+		root.setBottom(spin);
+		BorderPane.setAlignment(spin, Pos.BOTTOM_CENTER);
+		
 		
 		// Set Scene to the HBox display
-		Scene scene = new Scene(display);
+		Scene scene = new Scene(root);
 		
 		// Action event for when button is clicked
 		spin.setOnAction(e -> animationPlayer(slotOneView, slotTwoView, slotThreeView, spin, 2.0));
@@ -92,6 +103,14 @@ public class Slots extends Application{
 			throw new IllegalArgumentException("Missing bundled image in Slot_Machine package: " + imageName);
 		}
 		return new Image(resource.toExternalForm());
+	}
+	
+	private ImageView imageResize(ImageView view) {
+		view.setFitWidth(100);
+		view.setFitHeight(100);
+		view.setPreserveRatio(true);
+		
+		return view;
 	}
 
 }
--- src/Snake/Snake.java
+++ src/Snake/Snake.java
@@ -10,19 +10,16 @@ package Snake;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Snake extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		/**
-		 * Comments for planning about the steps of Snake
-		 * 1. Create the Window to display the game field and display it
-		 * 2. Display the snake body
-		 * 3. Apply movement logic to the body
-		 * 4. 
-		 */
+	
+		
 	}
 	
 	public static void main(String[] args) {
